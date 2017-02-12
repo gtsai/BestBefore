b73aa790b07a52b957e5961e7db37ffaaf036bc2
Grace Tsai <gtsai@uci.edu>
2017-02-11 22:32:33 -0800

    add button navigates to addItem activity

--- app/src/main/java/com/example/grace/bestbefore/MainActivity.java
+++ app/src/main/java/com/example/grace/bestbefore/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +20,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addItemBtn = (FloatingActionButton) findViewById(R.id.addItem);
+        addItemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, AddItem.class));
             }
         });
     }
